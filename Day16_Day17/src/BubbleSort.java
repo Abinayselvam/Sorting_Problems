ec21016eabb3defd81c68484935d3788ccf8fa2a
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-22 13:50:22 +0530

    Added the bubblesort functionality

---
+++ Day16_Day17/src/BubbleSort.java
@@ -0,0 +1,23 @@
+public  class BubbleSort {
+    public static void bubbleSort(int[] arr)
+    {
+        int length= arr.length;
+        boolean Swapped;
+        for(int i=0;i<length-1;i++)
+        {
+            Swapped=false;
+            for (int j=0;j<length-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp= arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    Swapped=true;
+                }
+            }
+            if(!Swapped) break;
+
+        }
+    }
+}
--- Day16_Day17/src/Main.java
+++ Day16_Day17/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Sorting problems");
+        int[] arr={3,2,9,5,4};
+        BubbleSort.bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
