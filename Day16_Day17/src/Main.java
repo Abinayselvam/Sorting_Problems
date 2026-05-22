b35617a403dcbbd298e0c463cfc380cf6bca1347
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-22 14:13:11 +0530

    Added the Insertion Sort feature

--- Day16_Day17/src/Main.java
+++ Day16_Day17/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Sorting problems");
+        int[] arr={3,2,9,5,4};
+        //here can call method which we sort
+        InsertionSort.insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
