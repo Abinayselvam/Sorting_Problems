e8804d1c6e47531af977c4a393fd4a51268b43db
AbinayaSelvam <sabinaya352@gmail.com>
2026-05-22 14:14:55 +0530

    Add the array input details for sorting

--- Day16_Day17/src/Main.java
+++ Day16_Day17/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to the Sorting problems");
+        System.out.println("Welcome to the Sorting problems");
+        int[] arr={3,2,9,5,4};
+        //here can call method which we sort
+        System.out.println(Arrays.toString(arr));
     }
 }
